907a5bd22db7546d26b204df272ec465cfff0452
karimskeet <mohamed.karim1-etu@etu.univh2c.ma>
2021-10-10 10:59:45 +0100

    Update_level0

---
+++ Level0/aff_last_param.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc > 1)
+	{
+		i = argc - 1;
+		j = 0;
+		while (argv[i][j])
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+	}
+	else
+		write(1, "\n", 1);
+}
