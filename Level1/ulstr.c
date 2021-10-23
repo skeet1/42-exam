d16d9b9ffffda2d531bb74ef0e972b03f8aff918
karimskeet <mohamed.karim1-etu@etu.univh2c.ma>
2021-10-23 15:07:31 +0100

    add

--- Level1/ulstr.c
+++ Level1/ulstr.c
@@ -13,11 +13,9 @@ int	main(int argc, char **argv)
 				argv[1][i] += 32;
 			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 				argv[1][i] -= 32;
+			write(1, &argv[1][i], 1);
 			i++;
 		}
-		i = -1;
-		while (argv[1][++i])
-			write(1, &argv[1][i], 1);
 		write(1, "\n", 1);
 	}
 	else
